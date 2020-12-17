90331d65ad7f2c1d319a1b1259baef68f25a7eb2
David Hartmann <hartmann.david76@gmail.com>
2020-12-17 20:17:39 +0100

    fix: Add docstrings to functions of plot_df.py module

--- plot_df.py
+++ plot_df.py
@@ -3,7 +3,25 @@ import numpy as np
 #color_lists = ['r','k','b','g','y','m','c','chartreuse', 'silver', 'lightstealblue', 'deepskyblue', 'cornflowerblue', 'limegreen', 'mediumblue', 'goldenrod', 'burlywood', 'navy', 'brown', 'teal', 'darkgray', 'slategrey', 'turquoise', 'navajowhite', 'lightgray', 'darkgreen', 'mocassin', 'steelblue']
 
 def plot_df_line(df, x_col, y_cols, title, x_label, y_label, line_plot_settings):
-    
+    """
+    INPUT:
+        - df:       the DataFrame which should be plotted as a line plot,
+        - x_col:    x-axis of the plot
+        - y_cols:   the y axis of the plot
+        - title:    provide a title as string for the plot
+        - x_label:  name of the x axis
+        - y_label:  name of the y axis
+        - line_plot_settings: Dictionray with plot_settings
+
+    OUTPUT:
+        - A line plot with a specially designed layout
+
+    AIM:
+        1.) Set the plot
+        2.) Prepare the legend
+        3.) Set the plot axes and title
+    """
+
     figure_size=line_plot_settings['figure_size']
     subplots=line_plot_settings['subplots']
     fontsize_title=line_plot_settings['fontsize_title']
@@ -32,7 +50,7 @@ def plot_df_line(df, x_col, y_cols, title, x_label, y_label, line_plot_settings)
     legend_move =line_plot_settings['legend_move']
     legend_x=line_plot_settings['legend_x']
     legend_y=line_plot_settings['legend_y']
-            
+
     """
     Set plot
     """
@@ -60,7 +78,7 @@ def plot_df_line(df, x_col, y_cols, title, x_label, y_label, line_plot_settings)
         axes.get_legend().set_bbox_to_anchor((legend_x,legend_y))
 
     """
-    Set plot axes
+    Set plot axes and title
     """
     axes.set_title(title, size=fontsize_title, verticalalignment='bottom',horizontalalignment='center')
     if set_xticks_range == True:
@@ -77,9 +95,27 @@ def plot_df_line(df, x_col, y_cols, title, x_label, y_label, line_plot_settings)
     if ylim_value_lower != None or ylim_value_upper != None:
         axes.set_ylim(ylim_value_lower, ylim_value_upper)
     #axes.grid(True)
-    
-    
-def plot_df_bar(df, title, bar_setting_dict, subplots=(1,2), coord=0):
+
+
+def plot_df_bar(df, title, bar_setting_dict):
+    """
+    INPUT:
+        - df:       the DataFrame which should be plotted as a bar plot,
+                    index of df --> categorical x axis of the bar plot,
+                    columns of df --> the bars of the plot
+        - title:    provide a title as string for the plot
+        - bar_setting_dict: Dictionray with plot_settings
+
+    OUTPUT:
+        - A bar plot with a specially designed layout
+
+    AIM:
+        1.) Set the plot
+        2.) Prepare the legend
+        3.) Set the plot axes and title
+    """
+
+
     """
     Set plot
     """
@@ -92,7 +128,7 @@ def plot_df_bar(df, title, bar_setting_dict, subplots=(1,2), coord=0):
             align=bar_setting_dict['align'],
             legend=bar_setting_dict['legend_state']
            )
-    
+
     """
     Prepare legend
     """
@@ -105,9 +141,9 @@ def plot_df_bar(df, title, bar_setting_dict, subplots=(1,2), coord=0):
             axes.get_legend().set_bbox_to_anchor((bar_setting_dict['legend_x'], bar_setting_dict['legend_y']))
 
     """
-    Set plot axes
+    Set plot axes and title
     """
- 
+
     axes.set_title(title, size=bar_setting_dict['fontsize_title'], verticalalignment='bottom', horizontalalignment='center')
     if bar_setting_dict['set_yticks_range'] == True:
         axes.set_yticks(np.arange(bar_setting_dict['yticks_start'], bar_setting_dict['yticks_end'], bar_setting_dict['yticks_step']))
@@ -116,30 +152,46 @@ def plot_df_bar(df, title, bar_setting_dict, subplots=(1,2), coord=0):
         axes.set_xlabel(bar_setting_dict['x_label'], fontsize=bar_setting_dict['fontsize_axes_label'])
     else:
         axes.set_xlabel('')
-    
+
     if bar_setting_dict['y_label'] != '':
         axes.set_ylabel(bar_setting_dict['y_label'], fontsize=bar_setting_dict['fontsize_axes_label'])
     else:
         axes.set_ylabel('')
-  
-    
-    
-def plot_df_pie(df, title, explode, pie_chart_settings):
-    
-    figsize=pie_chart_settings['figsize']
-    shadow=pie_chart_settings['shadow']
-    autopct=pie_chart_settings['autopct']
-    startangle=pie_chart_settings['startangle']
-    fontsize_title=pie_chart_settings['fontsize_title']
-    fontsize_text=pie_chart_settings['fontsize_text']
-    fontsize_legend=pie_chart_settings['fontsize_legend']
-    legend_state=pie_chart_settings['legend_state']
-    legend_title=pie_chart_settings['legend_title']
-    legend_list_to_plot =pie_chart_settings['legend_list_to_plot']
-    legend_move = pie_chart_settings['legend_move']
-    legend_x=pie_chart_settings['legend_x']
-    legend_y=pie_chart_settings['legend_y']
-            
+
+
+
+def plot_df_pie(df, title, explode, pie_setting_dict):
+    """
+    INPUT:
+        - df: the DataFrame which should be plotted as a pie plot,
+                columns of df --> the pie pieces of the plot
+        - title: provide a title as string for the plot
+        - explode: a list which sets the explosion of each pie piece, length must be the same as the number of pie pieces
+        - pie_setting_dict:  Dictionray with plot_settings
+
+    OUTPUT:
+        - A pie chart with a specially designed layout
+
+    AIM:
+        1.) Set the plot
+        2.) Prepare the legend
+        3.) Set the plot axes and title
+    """
+
+    figsize=pie_setting_dict['figsize']
+    shadow=pie_setting_dict['shadow']
+    autopct=pie_setting_dict['autopct']
+    startangle=pie_setting_dict['startangle']
+    fontsize_title=pie_setting_dict['fontsize_title']
+    fontsize_text=pie_setting_dict['fontsize_text']
+    fontsize_legend=pie_setting_dict['fontsize_legend']
+    legend_state=pie_setting_dict['legend_state']
+    legend_title=pie_setting_dict['legend_title']
+    legend_list_to_plot =pie_setting_dict['legend_list_to_plot']
+    legend_move = pie_setting_dict['legend_move']
+    legend_x=pie_setting_dict['legend_x']
+    legend_y=pie_setting_dict['legend_y']
+
     """
     Set plot
     """
@@ -148,18 +200,18 @@ def plot_df_pie(df, title, explode, pie_chart_settings):
             explode=explode,
             autopct=autopct,
             textprops={'fontsize': fontsize_text},
-            shadow=shadow, 
+            shadow=shadow,
             startangle=startangle,
             figsize=figsize,
             legend=legend_state
            )
-    
+
     """
     Prepare legend
     """
-  
+
     if legend_state==True:
-        
+
         if legend_list_to_plot != '':
             axes.legend(legend_list_to_plot)
         if legend_title != '':
@@ -169,14 +221,10 @@ def plot_df_pie(df, title, explode, pie_chart_settings):
             axes.get_legend().set_bbox_to_anchor((legend_x,legend_y))
 
     """
-    Set plot axes
+    Set plot axes and title
     """
     axes.set_xlabel('')
     axes.set_ylabel('')
-    
+
     axes.set_title(title, size=fontsize_title, verticalalignment='bottom',horizontalalignment='center')
-    axes.axis('equal') 
-
-    
-   
-
+    axes.axis('equal')
